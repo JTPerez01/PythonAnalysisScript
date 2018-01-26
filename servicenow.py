c5c4194d5e02453f9d40f66620d9c29e1fae21c8
Joshua Thomas Perez <joshuathomasperez@gmail.com>
2018-01-26 07:51:55 -0600

    Update servicenow.py

--- servicenow.py
+++ servicenow.py
@@ -2,7 +2,7 @@
 #####################TO DO#####################
 # Add ability to create a ticket and populate fields 
 # Take input for the Caller (will change)
-  Thiss will populate User ID with CNP ID, add the Afected User as self and change Location to EC/DC
+# Thiss will populate User ID with CNP ID, add the Afected User as self and change Location to EC/DC
 # Set Priority level (at the discresion of the caller)
 # Assign the team Security - Cybersecurity (will always be)
 # Assign to proper incident label (Can get from main program choice)
