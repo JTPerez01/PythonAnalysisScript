ee2aea1d57c15c11f4882bd6ea2102ced94aeb7e
Joshua Thomas Perez <joshuathomasperez@gmail.com>
2018-01-30 06:44:23 -0600

    Update servicenow.py

--- servicenow.py
+++ servicenow.py
@@ -10,5 +10,43 @@
 # Populate work notes with the report generated from the AnalysisScript
 # 
 
-import requests
 import json
+import requests
+from requests.auth import HTTPBasicAuth
+
+# using local http proxy to log requests and responses
+proxies = {
+    "http": "http://127.0.0.1:8888",
+    "https": "https://127.0.0.1:8888",
+}
+
+auth = HTTPBasicAuth("admin", "Center1!")
+uri = "https://dev17860.service-now.com/incident.do?JSONv2"
+
+# define http headers for request
+headers = {
+    "Accept": "application/json;charset=utf-8",
+    "Content-Type": "application/json"
+}
+
+# define payload for request, note we are passing the sysparm_action variable in the body of the request
+# http://wiki.servicenow.com/index.php?title=JSONv2_Web_Service#insert
+#the person who is calling and being assigned to must already be in the system
+payload = {
+    'sysparm_action': 'insert',
+    'category': 'software',
+    'impact': '1',
+    'urgency': '1',
+    'work_notes': 'add work notes',
+    'comments': 'add comments',
+    'short_description': 'Phishing - User Reported CNPDIP',
+    'cmdb_ci': 'Email',
+    'caller_id': 'Joshua Perez'
+    'assigned_to': 'Joshua Sutfin'
+}
+
+r = requests.post(url=uri, data=json.dumps(payload), auth=auth, proxies=proxies, verify=False, headers=headers)
+content = r.json()
+assert (r.status_code == 200)
+print "Response Status Code: " + str(r.status_code)
+print "Response JSON Content: " + str(content)
